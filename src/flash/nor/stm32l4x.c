ff8c4a37fe99d66dd22479e3e9c96fb482f46acc
Zuyi Hu <hzy068808@gmail.com>
2026-09-07 22:02:35 +0800

    flash/stm32l4x: Support STM32U33/U34x devices
    This device is similar to STM32U37/U38x devices but with flash size
    equal to 256Kb
    
    Signed-off-by: HAOUES Ahmed <ahmed.haoues@st.com>
    Reviewed-by: Tarek BOCHKATI <tarek.bouchkati@st.com>
    
    (cherry picked from commit cb0ac0024b93050c91ee087dc87e078942f486e5)

--- src/flash/nor/stm32l4x.c
+++ src/flash/nor/stm32l4x.c
@@ -436,6 +436,10 @@ static const struct stm32l4_rev stm32wl4xx_revs[] = {
 	{ 0x1000, "A" },
 };
 
+static const struct stm32l4_rev stm32u33_u34xx_revs[] = {
+	{ 0x1000, "A" }, { 0x1001, "Z" }, { 0x1003, "Y" },
+};
+
 static const struct stm32l4_part_info stm32l4_parts[] = {
 	{
 	  .id                    = DEVID_STM32L47_L48XX,
@@ -885,6 +889,18 @@ static const struct stm32l4_part_info stm32l4_parts[] = {
 	  .flash_regs_base       = 0x48100000,
 	  .fsize_addr            = 0x0BF86F98,
 	  .otp_base              = 0x0BF86800,
+	  .otp_size              = 512,
+	},
+	{
+	  .id                    = DEVID_STM32U33_U34XX,
+	  .revs                  = stm32u33_u34xx_revs,
+	  .num_revs              = ARRAY_SIZE(stm32u33_u34xx_revs),
+	  .device_str            = "STM32U33/U34xx",
+	  .max_flash_size_kb     = 256,
+	  .flags                 = F_HAS_DUAL_BANK | F_HAS_TZ | F_HAS_L5_FLASH_REGS | F_WRP_HAS_LOCK,
+	  .flash_regs_base       = 0x40022000,
+	  .fsize_addr            = 0x0BFA07A0,
+	  .otp_base              = 0x0BFA0000,
 	  .otp_size              = 512,
 	},
 };
@@ -2323,6 +2339,7 @@ static int stm32l4_probe(struct flash_bank *bank)
 	case DEVID_STM32U3B_U3CXX:
 	case DEVID_STM32U37_U38XX:
 	case DEVID_STM32U35_U36XX:
+	case DEVID_STM32U33_U34XX:
 		page_size_kb = 4;
 		num_pages = flash_size_kb / page_size_kb;
 		stm32l4_info->bank1_sectors = num_pages;
